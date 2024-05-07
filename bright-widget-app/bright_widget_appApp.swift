5a2e690fb47d301eee94ddd2ccd369cadedc9b68
Luka Vujnovac <84441240+lukavujnovac@users.noreply.github.com>
2024-05-07 18:51:25 +0200

    Initial Commit

---
+++ bright-widget-app/bright_widget_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bright_widget_appApp.swift
+//  bright-widget-app
+//
+//  Created by Luka Vujnovac on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct bright_widget_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
